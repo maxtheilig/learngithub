ad6c827827b2b139509bd55959b5ca12ac9eecce
maxtheilig <max.theilig@live.de>
2016-05-15 19:24:28 +0200

    Add files via upload

---
+++ prime.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+
+void primeFactorisation(int);
+
+int main()
+{
+	int x;
+	int flag=0;
+	std::cout << "Enter a number: ";
+	std::cin >> x;
+	for (int i = 2; i < x/2; ++i)
+	{
+		if (x%i==0)
+		{
+			flag=1;
+			break;
+		}
+	}
+	if (flag==0)
+		std::cout << x << " is a prime number." << std::endl;
+	else
+		{
+		std::cout << x << " is not a prime number." << std::endl;
+		primeFactorisation(x);
+		}
+	return 0;
+}
+
+void primeFactorisation(int x)
+{
+	int div = 2;
+	while(x!=0)
+	{
+		if(x%div!=0)
+			++div;
+		else
+		{
+			x = x / div;
+			std::cout << div << ", ";
+			if(x==1)
+				break;
+		}
+	}
+}
