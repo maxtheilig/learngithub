ce40be41b6968e2c04eb7ddfe8a75ef338c2ffda
maxtheilig <max.theilig@live.de>
2016-06-07 10:41:41 +0200

    delegated tasks to functions

--- Theilig/day1/task4.cpp
+++ Theilig/day1/task4.cpp
@@ -1,47 +1,66 @@
 #include <iostream>
+#include <cmath>
+#include <climits>
 
 void primeFactorisation(int);
+bool isPrime(const unsigned int x);
+int getValue();
+void checkIfNumberIsPrimeAndIfNotPrintItsPrimeFactorisation(unsigned int x);
 
 int main()
 {
-	int x;
-	int flag=0;
-	std::cout << "Enter a number: ";
-	std::cin >> x;
-	for (int i = 2; i <= x/2; ++i){
-		if (x%i==0)
-		{
-			flag=1;
-			break;
-		}
-	}
-	if (x==1)
-		std::cout << 1 << "is not a prime number." << std::endl;
-	else if (flag==0)
-		std::cout << x << " is a prime number." << std::endl;
-	else
-		{
-		std::cout << x << " is not a prime number." << std::endl;
-		primeFactorisation(x);
-		}
+	checkIfNumberIsPrimeAndIfNotPrintItsPrimeFactorisation(getValue());
 	return 0;
+}
+
+int getValue()
+{
+	std::cout << "Enter a positive number: ";
+	unsigned int x;
+	std::cin >> x;
+	if(x==UINT_MAX)
+		std::cout << "Error: number is too big" << std::endl;
+	return x;
+}
+
+bool isPrime(const unsigned int x)
+{
+	if(x==1)
+		return false;
+	for(unsigned int i=2; i<sqrt(x); ++i)
+	{
+		if(x%i==0)
+			return false;
+	}
+	return true;
 }
 
 void primeFactorisation(int x)
 {
 	std::cout << "The number can be factorised to: ";
-	int div = 2;
+	unsigned int div = 2;
 	while(x!=0)
 	{
 		if(x%div!=0)
 			++div;
 		else
 		{
-			x = x / div;
+			x /= div;
 			std::cout << div << " ";
 			if(x==1)
 				break;
 		}
 	}
 	std::cout << std::endl;
+}
+
+void checkIfNumberIsPrimeAndIfNotPrintItsPrimeFactorisation(unsigned int x)
+{
+	if(isPrime(x))
+		std::cout << x << " is prime." << std::endl;
+	else
+	{
+		std::cout << x << " is not prime." << std::endl;
+		primeFactorisation(x);
+	}
 }
