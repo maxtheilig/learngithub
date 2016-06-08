5eeae341ea3e25be90b7ee836b473de6d49e1783
maxtheilig <max.theilig@live.de>
2016-06-08 17:12:52 +0200

    handled overflow & negative number input

--- Theilig/day1/factorial.cpp
+++ Theilig/day1/factorial.cpp
@@ -1,21 +1,40 @@
 #include <iostream>
+#include <climits>
+#include <stdlib.h>
 
-int factorial(int x)
+unsigned int factorial(unsigned int x)
 {
-	int y=1;
+	unsigned int result = 1;
 	while(x > 0)
 	{
-		y = y * x;
+		result *= x;
 		--x;
 	}
-	return y;
+	if(result == 0)
+	{
+		std::cout << "Warning: cannot display result because it is equal or bigger than the biggest allowed unsigned integer number: " << UINT_MAX << "." << std::endl;
+		exit (EXIT_FAILURE);
+	}
+	return result;
 }
-
+unsigned int getValue();
 int main()
 {
-	int x;
-	std::cout << "Enter a number: ";
-	std::cin >> x;
+	unsigned int x = getValue();
+	std::cout << "You entered: " << x << std::endl;
 	std::cout << x << "!=" << factorial(x) << std::endl;
 	return 0;
 } 
+
+unsigned int getValue()
+{
+	while(true)
+	{
+		int x;
+		std::cout << "Enter a POSITIVE number: ";
+		std::cin >> x;
+		if(x > 0)
+			return x;
+	}
+};
+		
