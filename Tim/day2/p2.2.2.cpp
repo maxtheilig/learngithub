71adf7fd22ed52992391c0d7043bfa9b3cdab7bd
Tim Feldenbreiter <tbfelder@tbfelder-LIFEBOOK-AH530>
2016-06-09 22:41:31 +0200

    matrixmaster

--- Tim/day2/p2.2.2.cpp
+++ Tim/day2/p2.2.2.cpp
@@ -1,5 +1,5 @@
-# include <iostream>
-
+#include <iostream>
+#include <cstdlib>
 
 int randnumb (unsigned const int maxnumber)
 {
@@ -7,66 +7,61 @@ int randnumb (unsigned const int maxnumber)
 	return output;
 }
 
-void matrixmultiplication( const int *matrixlhs, const int *matrixrhs, int *resultmatrix, unsigned const int colslhs, unsigned const int rowslhscolsrhs, unsigned const int rowsrhs)
+void matrixMultiplication( int *matrixlhs, int *matrixrhs, int *resultmatrix, unsigned const int colslhs, unsigned const int rowslhscolsrhs, unsigned const int rowsrhs)
 { 
-	for (unsigned int i=1, i <= colslhs, ++i) 
+	for (unsigned int i=1; i <= colslhs; ++i) 
 	{
-		for (unsigned int j=1, j <= rowsrhs, ++j)
+		for (unsigned int j=1; j <= rowsrhs; ++j)
 		{ 
-			for (unsigned int k=1 , k <= rowslhscolsrhs , ++k)
+			for (unsigned int k=1 ; k <= rowslhscolsrhs ; ++k)
 			{
-				*(resultmatrix+(i*rowsrhs+j))  +=  *(matrixlhs+(i-1)*rowslhscolsrhs+k) * *(matrixrhs+(k-1)*rowsrhs+j);
+				*(resultmatrix+(i-1)*rowsrhs+j)  +=  *(matrixlhs+(i-1)*rowslhscolsrhs+k) * *(matrixrhs+(k-1)*rowsrhs+j);
 			}
 		}
 	}
 }					
 
-void initMatrix( int *matrix, unsigned const int cols, unsigned const int rows, unsigned const int maxnumber)
+void initializeMatrix( int *matrix, unsigned const int cols, unsigned const int rows, unsigned const int maxnumber)
 {
-	for (unsigned int i=1, i <= cols, ++i) 
+	for (unsigned int i=1; i <= cols; ++i) 
         { 
-                for (unsigned int j=1, j <= rows, ++j)
+                for (unsigned int j=1; j <= rows; ++j)
                 {
-			*(matrix+(i*rows+j))=randnumb(maxnumber);
+			*(matrix+(i-1)*rows+j)=randnumb(maxnumber);
 		}
 	} 
 }
 
 void printMatrix( int *matrix, unsigned const int cols, unsigned const int rows)
 {
-	for (unsigned int i=1, i <= cols, ++i)   
+	for (unsigned int i=1; i <= cols; ++i)   
         {
-                std::cout  <<
-		for (unsigned int j=1, j <= rows, ++j)
+		for (unsigned int j=1; j <= rows; ++j)
                 {
-                        *(matrix+(i*rows+j))=randnumb(maxnumber);
+                        std::cout << *(matrix+(i-1)*rows+j) << '\t';
                 }
+		std::cout << std::endl;
         }
-
-
-
+	std::cout << '\n' << std::endl;
 }
+
 int main()
 {
-	unsigned const int colslhs = randnumb(5);
-	unsigned const int rowslhscolsrhs = randnumb(5);
-	unsigned const int rowsrhs = randnumb(5); 
-	int matrixlhs[colslhs][rowslhscolsrhs] = {0};
-	int matrixrhs[rowslhscolsrhs][rowsrhs] = {0};
-	int resultmatrix[colslhs][rowsrhs]= {0};
-	initMatrix( matrixlhs, colslhs, rowslhscolsrhs, 10);
-	initMatrix( matrixrhs, rowslhscolsrhs, rowsrhs, 10);
-	const int matrixlhs=matrixlhs;
-	const int matrixrhs=matrixrhs;
-	matrixmultiplication( matrixlhs, matrixrhs, resultmatrix, colslhs, rowslhscolsrhs, rowsrhs);
-	printMatrix(matrixlhs, colslhs, rowslhscolsrhs); printMatrix(matrixrhs, rowslhscolsrhs, rowsrhs); printMatrix(resultmatrix, colslhs, rowsrhs)
-	
-
-
-}
-
-
-
-
-
+	const unsigned int colslhs = 2;
+	const unsigned int rowslhscolsrhs = 4;
+	const unsigned int rowsrhs = 2;
  
+	int matrixlhs [colslhs][rowslhscolsrhs] = {0};
+	int matrixrhs [rowslhscolsrhs][rowsrhs] = {0};
+	int resultmatrix [colslhs][rowsrhs]= {0};
+
+	initializeMatrix( *matrixlhs, colslhs, rowslhscolsrhs, 4);
+	initializeMatrix( *matrixrhs, rowslhscolsrhs, rowsrhs, 4);
+	matrixMultiplication( *matrixlhs, *matrixrhs, *resultmatrix, colslhs, rowslhscolsrhs, rowsrhs);
+
+	printMatrix(*matrixlhs, colslhs, rowslhscolsrhs);
+	printMatrix(*matrixrhs, rowslhscolsrhs, rowsrhs); 
+	printMatrix(*resultmatrix, colslhs, rowsrhs);
+	
+	return 0;
+}
