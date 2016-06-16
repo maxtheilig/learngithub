3d8898602a718badaf717270198172dfe801c04c
tbfelder <tbfelder@users.noreply.github.com>
2016-06-16 13:58:10 +0200

    Update vector.h

--- Tim/day3/vector.h
+++ Tim/day3/vector.h
@@ -7,7 +7,26 @@
 class Vector
 {
   private:
+		int m_length;
+		double *m_vector;
   
   public:
+		Vector();
+		{
+			m_length = 1;
+			m_vector = 0.;      				// default constructor
+		}
+		
+		Vector( int length, double *array)			// user constructor
+		{
+			m_length=length;
+			for (unsigned int i=0; i<length; ++i)
+			{
+				m_vector[i]=array[i];
+			}
+		}
+
+		
+		
   
 };
